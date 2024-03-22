a1bec702ca2174f828551977482d6abcff5bcd82
Tit Kovalenko <t.a.kvlnk@gmail.com>
2024-03-22 23:01:32 +0200

    chore: update dist

--- dist/types/asyncSlices/asyncSlices.d.ts
+++ dist/types/asyncSlices/asyncSlices.d.ts
@@ -1,11 +1,11 @@
 import type { StateCreator, StoreMutatorIdentifier } from "zustand";
-import type { AsyncSliceCtx, AsyncSliceKeys, StateWithAsyncSlices } from "../types";
+import type { AsyncSliceCtx, AsyncSliceKeys } from "../types";
 declare module "zustand" {
     interface StoreMutators<S, A> {
-        "zustand/async-slices": A extends Record<string, unknown> ? StateWithAsyncSlices<S, A> : S;
+        "zustand/async-slices": S;
     }
 }
 export declare function asyncSlices<StoreState, Context extends Record<string, unknown>, Methods extends Record<AsyncSliceKeys<StoreState>, (this: AsyncSliceCtx<StoreState, Context>, ...params: never[]) => Promise<unknown>>, Mis extends [StoreMutatorIdentifier, unknown][] = [], Mos extends [StoreMutatorIdentifier, unknown][] = []>(stateCreator: StateCreator<StoreState, Mis, Mos, Omit<StoreState, AsyncSliceKeys<StoreState>>>, asyncMethods: Methods, additionalAsyncMethodContext?: Context): StateCreator<StoreState, Mis, [
     ...Mos,
-    ["zustand/async-slices", Methods]
+    ["zustand/async-slices", never]
 ], Omit<StoreState, AsyncSliceKeys<StoreState>>>;
