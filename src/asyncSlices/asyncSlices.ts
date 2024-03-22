739422775d180f94fa1870a7368f9caa932a81c8
Tit Kovalenko <t.a.kvlnk@gmail.com>
2024-03-22 22:30:20 +0200

    feat: improve state creator types

--- src/asyncSlices/asyncSlices.ts
+++ src/asyncSlices/asyncSlices.ts
@@ -1,19 +1,41 @@
-import type { StateCreator } from "zustand";
-import type { AsyncSliceCtx, AsyncSliceKeys } from "../types";
+import type { StateCreator, StoreMutatorIdentifier } from "zustand";
+import type { AsyncSliceCtx, AsyncSliceKeys, StateWithAsyncSlices } from "../types";
 import { createAsyncSliceBinder } from "../createAsyncSliceBinder/createAsyncSliceBinder";
+
+
+declare module "zustand" {
+  interface StoreMutators<S, A> {
+    "zustand/async-slices": A extends Record<string, unknown> ? StateWithAsyncSlices<S, A> : S;
+  }
+}
 
 export function asyncSlices<
   StoreState,
   Context extends Record<string, unknown>,
   Methods extends Record<
     AsyncSliceKeys<StoreState>,
-    (this: AsyncSliceCtx<StoreState, Context>, ...params: never[]) => Promise<unknown>
-  >
+    (
+      this: AsyncSliceCtx<StoreState, Context>,
+      ...params: never[]
+    ) => Promise<unknown>
+  >,
+  Mis extends [StoreMutatorIdentifier, unknown][] = [],
+  Mos extends [StoreMutatorIdentifier, unknown][] = []
 >(
-  stateCreator: StateCreator<Omit<StoreState, AsyncSliceKeys<StoreState>>>,
+  stateCreator: StateCreator<
+    StoreState,
+    Mis,
+    Mos,
+    Omit<StoreState, AsyncSliceKeys<StoreState>>
+  >,
   asyncMethods: Methods,
   additionalAsyncMethodContext = {} as Context
-): StateCreator<StoreState> {
+): StateCreator<
+  StoreState,
+  Mis,
+  [...Mos, ["zustand/async-slices", Methods]],
+  Omit<StoreState, AsyncSliceKeys<StoreState>>
+> {
   return (setState, getState, api) => {
     const bindAsyncSlice = createAsyncSliceBinder(api);
 
