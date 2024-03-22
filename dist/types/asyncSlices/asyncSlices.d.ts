9440f683d6d29be83797df49d40bf786da6d37bb
Tit Kovalenko <t.a.kvlnk@gmail.com>
2024-03-22 22:41:36 +0200

    chore: update dist

--- dist/types/asyncSlices/asyncSlices.d.ts
+++ dist/types/asyncSlices/asyncSlices.d.ts
@@ -1,3 +1,11 @@
-import type { StateCreator } from "zustand";
-import type { AsyncSliceCtx, AsyncSliceKeys } from "../types";
-export declare function asyncSlices<StoreState, Context extends Record<string, unknown>, Methods extends Record<AsyncSliceKeys<StoreState>, (this: AsyncSliceCtx<StoreState, Context>, ...params: never[]) => Promise<unknown>>>(stateCreator: StateCreator<Omit<StoreState, AsyncSliceKeys<StoreState>>>, asyncMethods: Methods, additionalAsyncMethodContext?: Context): StateCreator<StoreState>;
+import type { StateCreator, StoreMutatorIdentifier } from "zustand";
+import type { AsyncSliceCtx, AsyncSliceKeys, StateWithAsyncSlices } from "../types";
+declare module "zustand" {
+    interface StoreMutators<S, A> {
+        "zustand/async-slices": A extends Record<string, unknown> ? StateWithAsyncSlices<S, A> : S;
+    }
+}
+export declare function asyncSlices<StoreState, Context extends Record<string, unknown>, Methods extends Record<AsyncSliceKeys<StoreState>, (this: AsyncSliceCtx<StoreState, Context>, ...params: never[]) => Promise<unknown>>, Mis extends [StoreMutatorIdentifier, unknown][] = [], Mos extends [StoreMutatorIdentifier, unknown][] = []>(stateCreator: StateCreator<StoreState, Mis, Mos, Omit<StoreState, AsyncSliceKeys<StoreState>>>, asyncMethods: Methods, additionalAsyncMethodContext?: Context): StateCreator<StoreState, Mis, [
+    ...Mos,
+    ["zustand/async-slices", Methods]
+], Omit<StoreState, AsyncSliceKeys<StoreState>>>;
